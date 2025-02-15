13cf4ef062546b8d900d453a4d7414e426d701fd
IvanVentura143 <ivansopogiiii@gmail.com>
2025-02-15 23:33:47 +0800

    Add files via upload

---
+++ Science Dictionaryy.py
@@ -0,0 +1,41 @@
+data={
+    "Pressure": "The amount of force in a particular area or space",
+    "Instaneous speed": "The speed at a specific instant",
+    "gravity": "Is a force that attracts anything towards it",
+    "Acceleration": "It is the change in velocity of an object",
+    "First law of motion": "Inertia; an object at rest will remain at rest unless any external force is aplied. an object in motion will stay in motion uless any external force is applied",
+    "Second law of motion": "Acceleration Decelaration; it is the change in velocity",
+    "Third law of motion": "Action Reaction; in every action there will be always equal and opposite reaction",
+    "Convection current": "The hot magma goes up and the cold magma goes down",
+    "Work": "It is the force done at that made an object displace",
+    "Inelastic collision": "The colliding objects will stick to each other right after the collision",
+    "Elastic collision": "The colliding objects will bounce against each other right after the collision",
+    "Air resistance": "The amount force that is made by wind againts from the movement of an object",
+    "Normal force": "It is the force that is made by the ground against object at rest",
+    "Free fall": "The acceleration of an object due to gravity",
+    "Displacement":  "the shortest straight-line distance from the initial to the final position",
+    "Translational motion":  "The movement along he straight line",
+    "Light year": "It is the distance traveled by light in one year",
+    "Projectile motion": "It is the motion of an object that traveles in a curved path due to gravity",
+    "Potential energy": "It is the energy that is stored on an object",
+    "Kinetic energy": "it is the energy that an object has during its motion",
+    "Electricity": " Is a form of energy which obtained from electric current",
+    "Friction": "Is a force that oposes the motion of two surfacess",
+    "Ionization energy": "Refers to the energy needed to remove an electron from an atom or molecule",
+    "Electronegativity": "Refers to the tendency of an atom to attarct the electrons in chemical bond",
+    "Matter": "Anything that occupies space and has mass",
+    "Scalar quantity": "Described by a ingle number with a unit clled magnitude",
+    "Vector quantity": "It is described by magiue and direction",
+    "Candela": "the base unit of light",
+    "Fahrenheit": "The base unit of temperature",
+    "Pounds":"The english unit of weight"
+    }
+print("SCIENCE DICTIONARY")
+a = input("search:")
+print(data[a])
+    
+
+    
+    
+
+    
